78e0a19d62cd78d13227cf5dcca914ade3e2c808
Riza Çelik <rizacelik@gmail.com>
2016-06-27 23:56:28 +0300

    Create view.php

---
+++ src/stencil/view.php
@@ -0,0 +1,18 @@
+<?php
+$content = <<<EOT
+<h2>Viewing <span class='muted'><?=\${$app_name}->{$id}?></span></h2>
+<br>
+EOT;
+
+foreach ($fields as $field):
+	$content .= "\t" . '<p><strong>' . ucwords(str_replace('_', ' ', $field)) . '</strong>' . PHP_EOL;
+	$content .= "\t<?=\$" . $app_name . '->' . $field . '?><p>' . PHP_EOL;
+endforeach;
+                
+$content .= <<<EOT
+<p>
+	<?= Html::anchor('$app_name/view/'.\${$app_name}->{$id}, 'View');?> |
+	<?= Html::anchor('$app_name', 'Back');?>
+</p>
+EOT;
+?>
